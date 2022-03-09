9c3dd45362c329ee88c31f15467821c946778918
anilyildiz <anilyildiz59@gmail.com>
2022-03-10 02:22:23 +0300

    modified filter input

--- src/app/pages/filter/filter.component.ts
+++ src/app/pages/filter/filter.component.ts
@@ -1,16 +1,10 @@
 import { Component, Inject } from '@angular/core';
-import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
+import { MatChipInputEvent, MatDialogRef, MatOptionSelectionChange, MAT_DIALOG_DATA } from '@angular/material';
 import { Category } from 'src/app/model/category';
 import { Publisher } from 'src/app/model/publisher';
 import { COMMA, ENTER } from '@angular/cdk/keycodes';
-
-export class FilterData {
-  constructor(
-    public categories: Category[],
-    public publishers: Publisher[],
-    public keywords: string[]
-  ) {}
-};
+import { FilterPayload } from 'src/app/model/dialog';
+import { FilterableModel } from 'src/app/model/base';
 
 @Component({
   selector: 'app-filter',
@@ -18,25 +12,26 @@ export class FilterData {
 })
 export class FilterComponent {
 
-  categories: number[] = this.data.categories.filter(c => c.selected).map(c => c.id);
-  publishers: number[] = this.data.publishers.filter(p => p.selected).map(p => p.id);
-  keywords: string[] = JSON.parse(JSON.stringify(this.data.keywords));
+  selectedCategories = this.input.categories.filter(c => c.visible);
+  selectedPublishers = this.input.publishers.filter(p => p.visible);
+
+  keywords: string[] = this.input.keywords;
   readonly separatorKeysCodes: number[] = [ENTER, COMMA];
   
   constructor(
     public dialogRef: MatDialogRef<FilterComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: FilterData
+    @Inject(MAT_DIALOG_DATA) public input: FilterPayload
   ) { }
 
-  add(event: any) {
+  add(event: MatChipInputEvent) {
     let input = event.input;
     let value = event.value;
 
-    if((value || '').trim()) {
-      this.keywords.push(value.trim());
+    if (value && value.trim().length > 0) {
+      this.keywords.push(value.trim().toLowerCase());
     }
 
-    if(input) {
+    if (input) {
       input.value = '';
     }
   }
@@ -47,14 +42,13 @@ export class FilterComponent {
   }
 
   reset() {
-    this.categories = this.data.categories.map(c => c.id);
-    this.publishers = this.data.publishers.map(p => p.id);
+    this.selectedCategories = this.input.categories;
+    this.selectedPublishers = this.input.publishers;
     this.keywords = [];
   }
 
   submit() {
-    this.dialogRef.close({
-      categories: this.categories, publishers: this.publishers, keywords: this.keywords
-    });
+    this.dialogRef.close(new FilterPayload(this.selectedCategories, this.selectedPublishers, this.keywords));
   }
+  
 }
